000f83c9ae3291d474aeee698f26a7846e0da5ed
niraj-jha <jhaniraj45@gmail.com>
2024-12-12 22:00:27 +0530

    First commit for The Coder'z Quest

---
+++ client/src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import Layout from './pages/Layout'
+import { HomePage } from './pages/HomePage'
+
+function App() {
+	return (
+		<Routes>
+			<Route path='/' element={<Layout />}>
+				<Route index element={<HomePage />} />
+			</Route>
+		</Routes>
+	)
+}
+
+export default App;
