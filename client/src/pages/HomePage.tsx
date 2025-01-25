3b62f620fe6fb4c14af5434faddb6b358f83c0cb
niraj-jha <jhaniraj45@gmail.com>
2025-01-26 00:10:51 +0530

    Another commit for checking

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -4,6 +4,7 @@ export const HomePage = () => {
             <div className="bg-red-500">HomePage</div>
             <h1>First commit by Pandey Jii</h1>
             <p>Commit by Niraj Jha</p>
+            <p>Another commit for checking</p> 
         </>
     )
 }
