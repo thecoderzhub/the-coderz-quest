859143061d8185208c229ae6a9dd2d4f1dd8487a
harry <garima98pandey04@gmail.com>
2025-01-25 18:19:06 +0530

    First commit by Harsh

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,5 +1,8 @@
 export const HomePage = () => {
     return (
-        <div className="bg-red-500">HomePage</div>
+        <>
+            <div className="bg-red-500">HomePage</div>
+            <h1>First commit by Pandey Jii</h1>
+        </>
     )
 }
